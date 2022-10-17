b4b009980b08b38a072fbb38e171f23da85aafce
RTS-1989 <63114573+RTS-1989@users.noreply.github.com>
2022-10-17 10:55:27 +0500

    Added docstrings in db_finctions added methods for DbCreator and DbFunctions

--- db/db_functions.py
+++ db/db_functions.py
@@ -21,6 +21,8 @@ class DbConnector:
 
     @contextmanager
     def _get_cursor(self):
+        """Context manager for cursor. Used in _execute function"""
+
         cursor, conn = None, None
         try:
             conn = self.conn_pool.getconn()
@@ -32,9 +34,11 @@ class DbConnector:
             cursor.close()
             self.conn_pool.putconn(conn)
 
-    def _execute(self, query, values: tuple = None):
+    def _execute(self, query, *args):
+        """Custom execute function with context manager"""
+
         with self._get_cursor() as cur:
-            cur.execute(query, values)
+            cur.execute(query, args)
             cur.execute("COMMIT")
 
 
@@ -44,17 +48,29 @@ class DbCreator(DbConnector):
         super().__init__(db_connect_info)
 
     def create_users_table(self):
+        """Creates table expenses_bot_user"""
+
         query = """
         CREATE TABLE IF NOT EXISTS expenses_bot_user(
-        id SERIAL NOT NULL PRIMARY KEY, name VARCHAR(100), last_name VARCHAR(100), email VARCHAR(100), 
+        id SERIAL PRIMARY KEY, name VARCHAR(100), last_name VARCHAR(100), email VARCHAR(100), 
         telegram_id NOT NULL UNIQUE INTEGER)
         """
         self._execute(query=query)
 
     def create_category_table(self):
+        """Creates table expenses_category"""
+
         query = """
         CREATE TABLE IF NOT EXISTS expenses_category(
-        id SERIAL NOT NULL PRIMARY KEY, category_name VARCHAR(100);
+        id SERIAL PRIMARY KEY, category_name VARCHAR(100);
+        """
+        self._execute(query=query)
+
+    def create_currency_table(self):
+        """Creates table currency"""
+
+        query = """
+        CREATE TABLE IF NOT EXISTS currency(id SERIAL PRIMARY KEY, currency_name VARCHAR(100))
         """
         self._execute(query=query)
 
@@ -63,9 +79,10 @@ class DbCreator(DbConnector):
         These words' user can write in his message and this word checks in database. If there is not such word
         telegram bot suggest adding this word in table expenses_dictionary and add category for this word.
         """
+
         query = """
         CREATE TABLE IF NOT EXISTS expenses_dictionary(
-        id SERIAL NOT NULL PRIMARY KEY, 
+        id SERIAL PRIMARY KEY, 
         word VARCHAR(100),
         category_id INTEGER
         FOREIGN KEY category_id REFERENCES expenses_category(id) ON DELETE RESTRICT;
@@ -74,13 +91,16 @@ class DbCreator(DbConnector):
         self._execute(query=query)
 
     def create_expenses_table(self):
+        """Creates table expenses"""
+
         query = """
         CREATE TABLE IF NOT EXISTS expenses(
-        id SERIAL NOT NULL PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         expenses_sum FLOAT,
-        currency VARCHAR(100),
+        currency_id NOT NULL INTEGER,
         category_id NOT NULL INTEGER,
         user_id NOT NULL INTEGER
+        FOREIGN KEY currency_id REFERENCES currency(id) ON DELETE RESTRICT
         FOREIGN KEY category_id REFERENCES expenses_category(id) ON DELETE RESTRICT 
         FOREIGN KEY user_id REFERENCES expenses_bot_user(id) ON DELETE RESTRICT;
         )
@@ -88,6 +108,10 @@ class DbCreator(DbConnector):
         self._execute(query=query)
 
     def create_first_weekday_func(self):
+        """
+        Function to get first day of week. For get_expenses_by_week and get_expenses_by_category_for_week functions
+        """
+
         query = """
         CREATE OR REPLACE FUNCTION first_wd () RETURNS DATE AS $$
         DECLARE wd DATE;
@@ -100,6 +124,10 @@ class DbCreator(DbConnector):
         self._execute(query=query)
 
     def create_last_weekday_func(self):
+        """
+        Function to get last day of week. For get_expenses_by_week and get_expenses_by_category_for_week functions
+        """
+
         query = """
         CREATE OR REPLACE FUNCTION last_wd () RETURNS DATE AS $$
         DECLARE wd DATE;
@@ -112,6 +140,8 @@ class DbCreator(DbConnector):
         self._execute(query=query)
 
     def create_tables_and_functions(self):
+        """Creates all table and functions"""
+
         self.create_users_table()
         self.create_category_table()
         self.create_expenses_table()
@@ -124,17 +154,38 @@ class DbFunctions(DbConnector):
     def __init__(self, db_connect_info: Dict[str, Any]):
         super().__init__(db_connect_info)
 
-    def insert_expense(self, spending_sum: int, payment_currency: str, category_id: int, user_id):
+    def create_user(self, name: str, last_name: str, email: str, telegram_id: int):
+        """Inserts user entry into expenses_bot_user table"""
+        query = """
+        INSERT INTO expenses_bot_user(name, last_name, email, telegram_id) VALUES ($1, $2, $3, $4)
+        """
+        self._execute(query, name, last_name, email, telegram_id)
+
+    def insert_category(self, category_name: str):
+        """Inserts category entry into expenses_category table"""
+        query = """
+        INSERT INTO expenses_category(category_name) VALUES ($1)
+        """
+        self._execute(query, category_name)
+
+    def insert_currency(self, currency_name: str):
+        """Inserts currency entry into currency table"""
+        query = """
+        INSERT INTO currency(currency_name) VALUES ($1)
+        """
+        self._execute(query, currency_name)
+
+    def insert_expense(self, spending_sum: int, currency_id: int, category_id: int, user_id: int):
         """Insert expense into expenses table"""
         query = """
-        INSERT INTO expenses(expenses_sum, currency, category_id, user_id) VALUES ($1, $2, $3, $4)
+        INSERT INTO expenses(expenses_sum, currency_id, category_id, user_id) VALUES ($1, $2, $3, $4)
         """
-        self._execute(query, (spending_sum, payment_currency, category_id, user_id,))
+        self._execute(query, spending_sum, currency_id, category_id, user_id)
 
     def get_expenses_by_specific_day(self, day: datetime.date):
         """Expenses by specific day. For example 2022-10-10. Format for day is 2022-10-10"""
         query: str = """SELECT * FROM expenses WHERE date = $1"""
-        self._execute(query, (day,))
+        self._execute(query, day)
 
     def get_expenses_by_week(self):
         """Expenses by current week"""
